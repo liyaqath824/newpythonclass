50dcc7f422a2ca6a8ec4e61cc07a4e82f1b2c267
liyaqath824 <45966887+liyaqath824@users.noreply.github.com>
2020-03-04 23:30:48 -0800

    new homework
    abcd

---
+++ 1. Feb27/homework1.py
@@ -0,0 +1,10 @@
+userinput=input("Enter the First Name")
+print("Hey",userinput)
+
+Userinput=input("Enter the Last Name")
+print("Hey",userinput)
+
+Useroutput=First Name+Last Name
+print("Hey",useroutput)
+
+
